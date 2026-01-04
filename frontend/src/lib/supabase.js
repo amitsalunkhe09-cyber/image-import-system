89b54a891b59a1ff8506ffcbe6ec5ffe642f7239
Adi Salunkhe <amitsalunkhe769@gmail.com>
2026-01-04 12:13:12 +0530

    prepare frontend for clean vercle deployment

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -11,10 +11,3 @@ export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
 );
-//const SUPABASE_URL = "https://gwxquybueoytkyzkbhuh.supabase.co";
-//const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3eHF1eWJ1ZW95dGt5emtiaHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0MjQ1NzcsImV4cCI6MjA4MzAwMDU3N30.B2LD2USLdvkSKI3F3K0I4UpIWBZA0KTyB9LOmR5O81I";
-//
-//export const supabase = createClient(
-//  SUPABASE_URL,
-//  SUPABASE_ANON_KEY
-//);
